2d3f00de1e9ad9ed7cec776b5b50063a57646f62
JoakimChan <120197963+JoakimChan@users.noreply.github.com>
2024-09-06 00:42:13 +0200

    commit

--- public/views/about.js
+++ public/views/about.js
@@ -1,43 +1,89 @@
 export default () => /*html*/`
-  <div id="aboutMeText">
-    <h2>About me </h2>
+<div id="aboutMeSection">
+  <section id="aboutMeText">
+    <h2>About Me</h2>
     <p>
-      My name is Joakim and this is my online CV. This project serves as a demonstration of my basic web development skills,
-      showcasing single-page, mobile-responsive websites. It also offers insight into who I am for those interested in learning more about me.
-      <br>
-      <br>
-      Presently, I am based in Stockholm, where I am pursuing studies in the Software Tester program at a vocational university (KYH).
-      I am expected to complete this program by the end of May 2025.
+      My name is Joakim, and welcome to my online CV. This project showcases my fundamental web development skills, including
+      the creation of single-page, mobile-responsive websites. It also provides a deeper insight into who I am for those
+      interested in learning more.
     </p>
-  </div>
+    <p>
+      Currently, I reside in Stockholm, where I am pursuing studies in the Software Tester program at KYH Vocational University.
+      I expect to complete my program by the end of May 2025.
+    </p>
+  </section>
+</div>
 
- 
 
-  <div id="skillsOfMine">
-    <h2>Skills of mine</h2>
-    <div>
-      <div>
-        <p>Web development</p>
-        <p>All the things that come with web development: Javascript, Nodsjs, Responsive design, CSS</p>
-      </div>
+<div id="myEducations">
+  <section class="education-section">
+    <h2 class="section-title">Education</h2>
 
-      <div>
-        <p>Database</p>
-        <p>SQL, Rest API, MySQL, Data Modeling, Understanding of Database Concepts</p>
-      </div>
-
-      <div>
-        <p>Cypress</p>
-        <p>robust front-end automation testing tool that empowers developers to efficiently conduct end-to-end testing
-          of web applications.</p>
-      </div>
-
-      <div>
-        <p>Postman</p>
-        <p>Streamlines web API testing by enabling users to send requests, verify responses, and automate tests.
-          Equipped with request building and test scripting features, it guarantees API reliability and functionality.
-        </p>
-      </div>
+    <div class="education-item">
+      <h3 class="school-name">KYH Yrkeshögskola</h3>
+      <h4 class="degree-title">Software Testing</h4>
+      <p class="study-period">AUG 2023 - PRESENT</p>
+      <p class="study-description">
+        I have recently started the vocational university program for software testing, where I am studying teamwork and industry knowledge. Additionally, I have polished my foundational skills in JavaScript, HTML, and CSS.
+      </p>
     </div>
-  </div>
+
+    <div class="education-item">
+      <h3 class="school-name">Jönköping University</h3>
+      <h4 class="degree-title">Computer Technology: Software and Mobile Platform</h4>
+      <p class="study-period">AUG 2017 - PRESENT</p>
+      <p class="study-description">
+        I graduated in the summer of 2022 from Jönköping University in the program Computer Technology: Software and Mobile Platform, though I have a few unfinished courses.
+      </p>
+    </div>
+
+    <div class="education-item">
+      <h3 class="school-name">Jensen Yrkeshögskola</h3>
+      <h4 class="degree-title">COBOL Programmer</h4>
+      <p class="study-period">AUG 2014 - JUN 2015</p>
+      <p class="study-description">
+        I graduated from Jensen Yrkeshögskola in the COBOL Programmer program, where I learned to program in the COBOL language.
+      </p>
+    </div>
+
+    <div class="education-item">
+      <h3 class="school-name">Tullinge Gymnasium</h3>
+      <h4 class="degree-title">Natural Science Line with a Focus on Mathematics and Computer Technology</h4>
+      <p class="study-period">AUG 2011 - JUN 2014</p>
+      <p class="study-description">
+        During my high school period, I learned basic programming in Java and the fundamentals of code structure.
+      </p>
+    </div>
+  </section>
+</div>
+
+  <section id="skills">
+    <h2>My Skills</h2>
+
+    <div class="skill-item">
+      <h3>Web Development</h3>
+      <p>Proficient in all aspects of web development, including JavaScript, Node.js, responsive design, and CSS.</p>
+    </div>
+
+    <div class="skill-item">
+      <h3>Database Management</h3>
+      <p>Skilled in SQL, REST APIs, MySQL, data modeling, and a strong understanding of core database concepts.</p>
+    </div>
+
+    <div class="skill-item">
+      <h3>Cypress</h3>
+      <p>Expert in using Cypress for robust front-end automation, enabling efficient end-to-end testing of web applications.</p>
+    </div>
+
+    <div class="skill-item">
+      <h3>Postman</h3>
+      <p>Experienced in API testing with Postman, using its request-building, response-verification, and test automation features to ensure API reliability and functionality.</p>
+    </div>
+
+    <div class="skill-item">
+      <h3>Vitest</h3>
+      <p>Proficient in Vitest for fast and reliable unit testing in JavaScript/TypeScript environments, ensuring high-quality code.</p>
+    </div>
+  </section>
+
 `;
--- public/views/projects.js
+++ public/views/projects.js
@@ -1,4 +1,49 @@
 export default () => /*html*/`
     <h1>Projects</h1>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+     <section id="projects-section">
+    <div class="projects-container">
+      <div class="project-box">
+        <a href="https://github.com/JoakimChan/Questionnaire" target="_blank">
+            <h3>Questionnaire</h3>
+        </a>
+        <p>My first school project</p>
+        <div class="tags">
+          <span class="tag">Javascript</span>
+        </div>
+      </div>
+
+      <div class="project-box">
+        <a href="https://github.com/JoakimChan/MusicianAndBand" target="_blank">
+            <h3>MusicianAndBand</h3>
+        </a>
+        <p>Creative collaboration platform that combines video conferencing and HD media streaming</p>
+        <div class="tags">
+          <span class="tag">Javascript</span>
+        </div>
+      </div>
+
+      <div class="project-box">
+        <a href="https://github.com/JoakimChan/BrokerSite" target="_blank">
+            <h3>BrokerSite</h3>
+        </a>
+        <p>Browser extension that records everything happening in a web application</p>
+        <div class="tags">
+          <span class="tag">Javascript</span>
+          <span class="tag">Jason server</span>
+        </div>
+      </div>
+    </div>
+
+    <div class="project-box">
+        <a href="https://github.com/JoakimChan/TicTakToe" target="_blank">
+            <h3>TicTakToe</h3>
+        </a>
+        <p>Browser extension that records everything happening in a web application</p>
+        <div class="tags">
+          <span class="tag">Java</span>
+        </div>
+      </div>
+    </div>
+
+  </section>
 `;
